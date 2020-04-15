705c18691f1fded2425ae61b613d8e463701a1b8
c14180134 <c14180134@john.petra.ac.id>
2020-04-15 10:09:14 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -12,5 +12,34 @@ export class AppComponent  {
   hitung(x,b){
   this.hasil = Math.pow(x,b);
   };
+  i=0
+  c=0
+  polaA(x,b){
+    for(this.i=x;this.i<b;this.i++){
+      for(this.c=0;this.c<this.i;this.c++){
+        x='x'
+        document.writeln("x");
+      }
+      document.writeln("<br/>");
+    }
+  }
+  polaB(x,b){
+    var k=1
+    for(this.i=x;this.i>0;this.i--){
+      for(this.c=0;this.c<this.i;this.c++){
+      
+      
+        document.writeln("x");
+        
+      }
+      var m=0
+      while(m<k&&k<b ){
+        m++;
+        document.writeln("O")
+      }
+      document.writeln("<br/>");
+      k++;
+    }
+  }
 
 }
